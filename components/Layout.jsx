8e3a144cd8acf9d4595ebca3aa03a7344711cdc8
adnanerlansyah403 <adnanerlansyah403@gmail.com>
2022-09-29 16:01:41 +0700

    Add Styles to App

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,24 +1,28 @@
 import Head from 'next/head'
 import React from 'react'
 import { AppBar, Container, Toolbar, Typography } from "@material-ui/core"
+import useStyles from './../utils/styles';
 
 export default function Layout({ children }) {
+
+  const classes = useStyles();
+
   return (
     <div>
       <Head>
         <title>Next Amazona</title>
       </Head>
-      <AppBar position="static">
+      <AppBar position="static" className={`${classes.navbar}`}>
         <Toolbar>
             <Typography>
                 Amazona
             </Typography>
         </Toolbar>
       </AppBar>
-      <Container>
+      <Container className={`${classes.main}`}>
         {children}
       </Container>
-      <footer>
+      <footer className={`${classes.footer}`}>
         <Typography>
             All rights reserved. NextAmazona
         </Typography>
