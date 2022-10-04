4b79f9441a504ed1588c37de84d4b9e3f3566608
adnanerlansyah403 <adnanerlansyah403@gmail.com>
2022-10-04 15:58:46 +0700

    Create Sample Users In MongoDB

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -2,12 +2,15 @@ import nc from "next-connect";
 import data from './../../utils/data';
 import Product from './../../models/ProductModel';
 import db from './../../utils/db';
+import User from './../../models/UserModel';
 
 const handler = nc();
 
 handler.get( async (req, res) => {
 
     await db.connect();
+    await User.deleteMany();
+    await User.insertMany(data.users);
     await Product.deleteMany();
     await Product.insertMany(data.products);
     await db.disconnect();
